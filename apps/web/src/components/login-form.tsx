8f45e2fadcffec34a4e2b2163143228b43db89de
Lucas Patrick <tech.patrick.lsilva@gmail.com>
2024-11-29 20:41:50 -0300

    feat: login form ui

---
+++ apps/web/src/components/login-form.tsx
@@ -0,0 +1,88 @@
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { PasswordInput } from "@/components/ui/password-input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const formSchema = z.object({
+  username: z.string().min(2).max(50),
+  password: z.string(),
+});
+
+type FormSchema = z.infer<typeof formSchema>;
+
+export function LoginForm() {
+  const form = useForm<FormSchema>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+    },
+  });
+
+  async function onSubmit(values: FormSchema) {
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-4 max-w-3xl mx-auto"
+      >
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Usuário</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="Seu usuário"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <PasswordInput
+                  placeholder="Sua senha"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button
+          type="submit"
+          className="w-full"
+          // disabled={mutation.isPending}
+        >
+          {/* {mutation.isPending && (
+                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+              )} */}
+          {/* {mutation.isPending ? "Entrando..." : "Entrar"} */}
+          Entrar
+        </Button>
+      </form>
+    </Form>
+  );
+}
--- apps/web/src/pages/login.tsx
+++ apps/web/src/pages/login.tsx
@@ -1,9 +1,27 @@
+import { LoginForm } from "@/components/login-form";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 export function LoginPage() {
   return (
     <div className="flex items-center justify-center min-h-screen">
       <div className="w-full max-w-md">
-        Login Form
-        {/* <LoginForm /> */}
+        <Card>
+          <CardHeader>
+            <CardTitle>Entrar no sistema</CardTitle>
+            <CardDescription>
+              Insira seu usuário e senha para acessar sua conta.
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <LoginForm />
+          </CardContent>
+        </Card>
       </div>
     </div>
   );
